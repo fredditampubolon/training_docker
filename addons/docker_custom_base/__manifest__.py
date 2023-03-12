0905b355991aa5d9d93393e2ad4653b184003930
freddi tampubolon <freddi.hamonangan@ishangry.com>
2023-03-12 13:45:44 +0700

    push file project

---
+++ addons/docker_custom_base/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Training Custome User With Docker ENV',
+    'version': '1.1',
+    'category': 'Custom',
+    'author': 'Freddi Tampubolon',
+    'summary': 'Training Odoo with ENV Docker',
+    'description': """ Odoo14 with Docker ENV custom base module """,
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'application': True,
+    'installable': True,        
+    'auto_install': False
+}
